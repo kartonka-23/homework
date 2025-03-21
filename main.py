2f258d65be8c688d846424216a78df23f9625e31
Данило Криницький <karton>
2025-03-21 22:39:19 +0200

    task 4

--- main.py
+++ main.py
@@ -20,24 +20,38 @@ import random
 
 # task 3
 
-import random
-num = random.randint(1, 10)
-y = 3
-for i in range(3):
-	y -= 1
-	answ = int(input("write number between 1-10: "))
+# num = random.randint(1, 10)
+# y = 3
+# for i in range(3):
+# 	y -= 1
+# 	answ = int(input("write number between 1-10: "))
 	
-	if y == 0:
-		print("You are losed")
-		break
-	else:
-		print(f"you have {y} tries")
+# 	if y == 0:
+# 		print("You are losed")
+# 		break
+# 	else:
+# 		print(f"you have {y} tries")
 
-	if answ < num:
-		print("greater number")
-	elif answ > num:
-		print("smaller number")
-	elif answ == num:
-		print("You are guessed")
-		break
-	i += 1
+# 	if answ < num:
+# 		print("greater number")
+# 	elif answ > num:
+# 		print("smaller number")
+# 	elif answ == num:
+# 		print("You are guessed")
+# 		break
+# 	i += 1
+
+
+
+
+
+min_num = int(input("enter smaller number: "))
+max_num = int(input("enter larger number: "))
+# print(f"{max_num} {min_num}")
+norm_num = max_num - 1
+
+
+while min_num != norm_num:
+	min_num+=1
+	print(min_num)
+
