480c6e5fb00533b97e67ef7793c1ffe85e8b4021
Данило Криницький <karton>
2025-04-25 17:37:28 +0300

    Додав main.py

--- main.py
+++ main.py
@@ -1,45 +1,15 @@
-from datetime import datetime
+class Test(object):
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.kringe = kringe
+        
 
-class Task:
-    def __init__(self, title, description, deadline):
-        self.title = title
-        self.description = description
-        self.deadline = deadline
-        self.completed = False
+class ClassName(Test):
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.kringe2 = kringe2
 
-    def mark_completed(self):
-        self.completed = True
+    def pokazuha()
+        print("elites varvars with rage")
 
-    def __str__(self):
-        status = "Done" if self.completed else "Not done"
-        return f"{self.title} | {self.description} | Deadline: {self.deadline} | {status}"
-
-
-class TaskManager:
-    def __init__(self):
-        self.tasks = []
-
-    def add_task(self, title, description, deadline):
-        task = Task(title, description, deadline)
-        self.tasks.append(task)
-
-    def delete_task(self, title):
-        self.tasks = [task for task in self.tasks if task.title != title]
-
-    def mark_task_completed(self, title):
-        for task in self.tasks:
-            if task.title == title:
-                task.mark_completed()
-
-    def list_tasks(self):
-        for task in self.tasks:
-            print(task)
-
-
-if __name__ == "__main__":
-    manager = TaskManager()
-    manager.add_task("Math Homework", "Solve exercises 1-10", "2025-04-12")
-    manager.add_task("Buy groceries", "Milk, Bread, Eggs", "2025-04-11")
-    manager.list_tasks()
-    manager.mark_task_completed("Math Homework")
-    manager.list_tasks()
+# завдання було тільки "Описати як створити наслідування" або "Навести приклад наслідування", а не використати
